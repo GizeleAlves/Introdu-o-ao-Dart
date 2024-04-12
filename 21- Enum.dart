458d09e208e7103828cced3a6a24e04984bf3a27
GizeleAlves <gizele0alves@gmail.com>
2024-04-12 13:41:49 -0300

    Enum

---
+++ 21- Enum.dart
@@ -0,0 +1,25 @@
+//21- Enum
+
+enum Telas {
+  SplashScreen,
+  HomePage,
+  Login,
+}
+
+void main() {
+  Telas tela = Telas.HomePage;
+
+  switch (tela) {
+    case Telas.SplashScreen:
+      print('Logo');
+      break;
+
+    case Telas.HomePage:
+      print('Página Inicial');
+      break;
+
+    case Telas.Login:
+      print('Entrar');
+      break;
+  }
+}
